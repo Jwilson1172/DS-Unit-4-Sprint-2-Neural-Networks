b6ba25849cde0b63f8bc97d28047b04b498f1595
JWilson1172 <jw59615@gmail.com>
2020-05-11 18:47:25 -0400

    mod1 progress

--- package/perceptron.py
+++ package/perceptron.py
@@ -1,78 +1,42 @@
-from random import seed
-from random import randrange
-from csv import reader
-# code based in majority from https://machinelearningmastery.com/implement-perceptron-algorithm-scratch-python/
-class Perceptron():
+import numpy as np
 
-    # Calculate accuracy percentage
-    def accuracy_metric(self,actual, predicted):
-		correct = 0
-		for i in range(len(actual)):
-			if actual[i] == predicted[i]:
-			    correct += 1
-	    return correct / float(len(actual)) * 100.0
 
-    # Evaluate an algorithm using a cross validation split
-    def evaluate_algorithm(self,dataset, algorithm, n_folds, *args):
-	    folds = cross_validation_split(dataset, n_folds)
-	    scores = list()
-	    for fold in folds:
-		    train_set = list(folds)
-		    train_set.remove(fold)
-		    train_set = sum(train_set, [])
-		    test_set = list()
-		    for row in fold:
-			    row_copy = list(row)
-			    test_set.append(row_copy)
-			    row_copy[-1] = None
-		    predicted = algorithm(train_set, test_set, *args)
-		    actual = [row[-1] for row in fold]
-	        accuracy = accuracy_metric(actual, predicted)
-		    scores.append(accuracy)
-	    return scores
+class Perceptron:
+    def __init__(self, epochs=10):
+        self.epochs = epochs
 
-    # Make a prediction with weights
-    def predict(self,row, weights):
-	    activation = weights[0]
-	    for i in range(len(row)-1):
-		    activation += weights[i + 1] * row[i]
-	    return 1.0 if activation >= 0.0 else 0.0
+    def __sigmoid(self, x):
+        return (1 / (1 + np.exp(-x)))
 
-    # Estimate Perceptron weights using stochastic gradient descent
-    def train_weights(self,train, l_rate, n_epoch):
-	    weights = [0.0 for i in range(len(train[0]))]
-	    for epoch in range(n_epoch):
-		    for row in train:
-			    prediction = predict(row, weights)
-			    error = row[-1] - prediction
-			    weights[0] = weights[0] + l_rate * error
-			    for i in range(len(row)-1):
-				    weights[i + 1] = weights[i + 1] + l_rate * error * row[i]
-	    return weights
+    def __sigmoid_derivative(self, x):
+        dx = self.__sigmoid(x)
+        return dx * (1 - dx)
 
-    # Perceptron Algorithm With Stochastic Gradient Descent
-    def perceptron(self, train, test, l_rate, n_epoch):
-	    predictions = list()
-	    weights = train_weights(train, l_rate, n_epoch)
-	    for row in test:
-		    prediction = predict(row, weights)
-		    predictions.append(prediction)
-	    return(predictions)
+    def fit(self, weights, X, y):
+        """Fit training data
+        X : Training vectors, X.shape : [#samples, #features]
+        y : Target values, y.shape : [#samples]
+        """
 
-if __name__ == '__main__':
-    # Test the Perceptron algorithm on the sonar dataset
-    seed(1)
-    # load and prepare data
-    filename = 'sonar.all-data.csv'
-    dataset = load_csv(filename)
-    for i in range(len(dataset[0])-1):
-	    str_column_to_float(dataset, i)
-    # convert string class to integers
-    str_column_to_int(dataset, len(dataset[0])-1)
-    # evaluate algorithm
-    n_folds = 3
-    l_rate = 0.01
-    n_epoch = 500
-    scores = evaluate_algorithm(dataset, perceptron, n_folds, l_rate, n_epoch)
-    print('Scores: %s' % scores)
-    print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
+        for i in range(self.epochs):
+            # Weighted sum of inputs / weights
+            ws = np.dot(X, weights)
+            # Activate!
+            activated = self.__sigmoid(ws)
+            # Cac error
+            error = y - activated
+            # Update the Weights
+            adjustments = error * self.__sigmoid_derivative(ws)
+            weights += np.dot(X.T, adjustments)
+
+            print(f"Epoch - {i} ******* error - {error}%\n")
+        self.weights = weights
+        print(f" ***** ***** *****\nFinal error score of {error}")
+        return
+
+    def setWeights(self, weights):
+        self.weights = weights
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return self.__sigmoid(np.dot(X, self.weights))
