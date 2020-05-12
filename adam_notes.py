0d4411cd4b2d59213510b3c4c063504dd485b123
JWilson1172 <jw59615@gmail.com>
2020-05-12 15:44:25 -0400

    mod2

---
+++ adam_notes.py
@@ -0,0 +1,10 @@
+adam_params = {'alpha': 0.001,
+               'beta1': 0.9,
+               'beta2': 0.999,
+               'epsilon': 10e-8}
+# explination:
+# alpha - Also referred to as the learning rate or step size.
+# The proportion that weights are updated (e.g. 0.001).
+# Larger values (e.g. 0.3) results in faster initial learning before the rate is updated.
+# Smaller values
+# (e.g. 1.0E-5) slow learning right down during training
